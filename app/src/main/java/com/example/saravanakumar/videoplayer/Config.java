14026da1a52d1b9a9a5fe19e2dcd38f6a1df1c8e
Saravanakumar B <saravanakumar@mallow-tech.com>
2017-10-13 18:20:39 +0530

    YouTubePlayerView
    Youtube player view to play youtube video in our app using youtube video link.

---
+++ app/src/main/java/com/example/saravanakumar/videoplayer/Config.java
@@ -0,0 +1,14 @@
+package com.example.saravanakumar.videoplayer;
+
+/**
+ * Created by saravanakumar on 01/09/17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCFlps9eTnw7p0kfxbQ7mXuO5VODlA6ChA";
+
+}
